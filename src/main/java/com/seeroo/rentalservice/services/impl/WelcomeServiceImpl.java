98925225f0ed58e74f2bb10fa51f17324a73acbe
anasa <anas-aji-seeroo>
2022-04-25 14:56:21 +0530

    for loop added.

--- src/main/java/com/seeroo/rentalservice/services/impl/WelcomeServiceImpl.java
+++ src/main/java/com/seeroo/rentalservice/services/impl/WelcomeServiceImpl.java
@@ -3,6 +3,7 @@ package com.seeroo.rentalservice.services.impl;
 import com.seeroo.rentalservice.dto.UserDetailsDTO;
 import com.seeroo.rentalservice.services.WelcomeService;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,8 @@ public class WelcomeServiceImpl implements WelcomeService {
     static String cIndia = "India";
     static String myCompany = "Seeroo";
     private static final List<String> countries = new ArrayList<String>();
+    private static final List<String> districts =
+            new ArrayList<String>(Arrays.asList("644", "957", "913"));;
 
     public WelcomeServiceImpl() {
         getCountries();
@@ -22,6 +25,15 @@ public class WelcomeServiceImpl implements WelcomeService {
         String welcomeMessage = "";
         if (countries.contains(userDetailsDTO.getLocation())) {
             welcomeMessage = "Namaste!!! " + userDetailsDTO.getName() + " Welcome to java. ";
+            for (int i = 0; i <= districts.size(); i++) {
+                if (countries.indexOf(userDetailsDTO.getLocation()) == i)
+                    welcomeMessage =
+                            welcomeMessage
+                                    + userDetailsDTO.getLocation()
+                                    + " have "
+                                    + districts.get(i)
+                                    + " districts. ";
+            }
             if (myCompany.equals(userDetailsDTO.getCompany())) {
                 welcomeMessage = welcomeMessage + myCompany + " is Awesome.";
             }
